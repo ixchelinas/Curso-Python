eb1bba5ee169d9645ec80eb4d97667cea3048d19
Ixchel Pineda <ixchel.ventura@gmail.com>
2020-09-21 21:29:26 -0500

    Se agrega el concepto de funciones

---
+++ funciones.py
@@ -0,0 +1,15 @@
+def conversacion(mensaje):
+    print('Hola\nCómo estás')
+    print(mensaje)
+    print('Adios')
+
+opcion = int(input('Elige una opción (1,2,3): '))
+
+if opcion == 1:
+    conversacion('Elegiste la opción 1')
+elif opcion == 2:
+    conversacion('Elegiste la opción 2')
+elif opcion == 3:
+    conversacion('Elegista la opción 3')
+else:
+    print('Escribe la opción correcta')
