3cd12d452ca619e5b5e86fbbfbee58797d458966
Ixchel Pineda <ixchel.ventura@gmail.com>
2020-09-21 21:23:29 -0500

    se agregaron la logica condicionales

---
+++ condicionales.py
@@ -0,0 +1,6 @@
+edad = int(input("Escribe tu edad: "))
+if(edad > 17 ):
+    #pass ## palabra clave para indicar que después escribiras código
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
--- conversor.py
+++ conversor.py
@@ -1,8 +1,37 @@
-pesos = input("¿cuántos pesos colombianos tienes?: ")
-pesos = float(pesos)
-valor_dolar = 3875
-dolares = pesos/valor_dolar
-dolares = round(dolares,2)
-dolares = str(dolares)
+menu = """
+Bienvenido al conversor de monedas 💰 
 
-print("Tienes $"+dolares+" dolares")
+1 - Pesos colombianos
+2 - Pesos argentinos
+3 - Pesos mexicanos
+
+Elige una opcion: """
+
+opcion = input(menu)
+
+if opcion == '1':
+    pesos = input("¿cuántos pesos colombianos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 3875
+    dolares = pesos/valor_dolar
+    dolares = round(dolares,2)
+    dolares = str(dolares)    
+    print("Tienes $"+dolares+" dolares")
+elif opcion == '2':
+    pesos = input("¿cuántos pesos argentinos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 65
+    dolares = pesos/valor_dolar
+    dolares = round(dolares,2)
+    dolares = str(dolares)    
+    print("Tienes $"+dolares+" dolares")
+elif opcion == '3': 
+    pesos = input("¿cuántos pesos mexicanos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 21
+    dolares = pesos/valor_dolar
+    dolares = round(dolares,2)
+    dolares = str(dolares)    
+    print("Tienes $"+dolares+" dolares")
+else:
+    print('Ingresa una opción correcta')
