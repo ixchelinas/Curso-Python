90531f4290179606472c7984b1114bc2e062d8a2
Ixchel Pineda <ixchel.ventura@gmail.com>
2020-09-30 21:40:07 -0500

    break bajo condiciones

--- break_continue.py
+++ break_continue.py
@@ -1,9 +1,15 @@
 def run():
     #tres ejemplos de ciclo for
-    for contador in range(1000):
-        if contador % 2 != 0:
-            continue
-        print(contador)
+    # for contador in range(1000):
+    #     if contador % 2 != 0:
+    #         continue
+    #     print(contador)
+
+    for i in range(10000):
+        print(i)
+        if i == 5678:
+            break
+
 
 if __name__ == '__main__':
     run()
