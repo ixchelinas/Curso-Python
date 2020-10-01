fee9b29da379dc6c18fe39a5ce7ee589647106b9
Ixchel Pineda <ixchel.ventura@gmail.com>
2020-09-30 21:31:31 -0500

    imprimiendo las letras en mayúsculas

--- recorrer.py
+++ recorrer.py
@@ -1,7 +1,11 @@
 def run():
-    nombre = input('Escribre tu nombre: ')
-    for letra in nombre:
-        print(letra)
+    # nombre = input('Escribre tu nombre: ')
+    # for letra in nombre:
+    #     print(letra)
+
+    frase = input('Escribre una frase: ')
+    for caracter in frase:
+        print(caracter.upper())
 
 # Para cerrar un programa Ctrl+c
 if __name__ == '__main__':
