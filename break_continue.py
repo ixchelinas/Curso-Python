ed9b13bf714138c464dd22791182e4a31d7a8bc8
Ixchel Pineda <ixchel.ventura@gmail.com>
2020-09-30 21:36:52 -0500

    aprendiendo break y continue en ciclos

---
+++ break_continue.py
@@ -0,0 +1,9 @@
+def run():
+    #tres ejemplos de ciclo for
+    for contador in range(1000):
+        if contador % 2 != 0:
+            continue
+        print(contador)
+
+if __name__ == '__main__':
+    run()
