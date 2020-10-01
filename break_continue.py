456cb9447265477b6c9f6c62711bb400b526ed70
Ixchel Pineda <ixchel.ventura@gmail.com>
2020-09-30 21:41:35 -0500

    break en cadenas

--- break_continue.py
+++ break_continue.py
@@ -5,10 +5,16 @@ def run():
     #         continue
     #     print(contador)
 
-    for i in range(10000):
-        print(i)
-        if i == 5678:
+    # for i in range(10000):
+    #     print(i)
+    #     if i == 5678:
+    #         break
+
+    texto = input('Escribe un texto: ')
+    for letra in texto:
+        if letra == 'o':
             break
+        print(letra)
 
 
 if __name__ == '__main__':
