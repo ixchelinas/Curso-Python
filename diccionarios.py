116a8df35b8cb561051bc48c403261b7bce65727
Ixchel Pineda <ixchel.ventura@gmail.com>
2020-10-06 21:16:38 -0500

    imprimiendo según los metodos de dicciones

--- diccionarios.py
+++ diccionarios.py
@@ -4,6 +4,24 @@ def run():
         "llave2" : 2,
         "llave3" : 3
     }
-    print(mi_diccionario)
-def if __name__ == "__main__":
+    # print(mi_diccionario)
+
+    poblacion_paises = {
+        'Argentina' : 44938912,
+        'Brazil' : 210147125,
+        'Colombia' : 50372424
+    }
+
+    # print(poblacion_paises['Argentina'])
+
+    # for pais in poblacion_paises.keys():
+    #     print(pais)
+
+    # for pais in poblacion_paises.values():
+        # print(pais)
+    
+    for pais, poblacion in poblacion_paises.items():
+        print(pais +' tiene '+str(poblacion)+' habitantes')
+
+if __name__ == '__main__':
     run()
