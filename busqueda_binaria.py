a830d82b26ecd2b1467af5d013145eb98d5bdc67
Ixchel Pineda <ixchel.ventura@gmail.com>
2020-10-13 15:43:48 -0500

    newton-raphson metodos numericos

---
+++ busqueda_binaria.py
@@ -0,0 +1,15 @@
+objetivo = int(input('Escoge un numero: '))
+epsilon = 0.01
+bajo = 0.0
+alto = max(1.0, objetivo)
+respuesta  = (alto + bajo)/2
+
+while abs(respuesta**2 - objetivo) >= epsilon:
+    print(f'bajo = {bajo}, alto={alto},respuesta = {respuesta}')
+    if respuesta**2 < objetivo:
+        bajo = respuesta
+    else:
+        alto = respuesta
+    respuesta = (alto + bajo)/2
+
+print('La raiz cuadrada de :'+str(objetivo)+' es '+str(respuesta))
